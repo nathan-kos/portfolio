ab69995ad37fa7959641db5ef6ac6aab2261d8c2
nathan ks <nathanks0210@gmail.com>
2024-11-06 15:47:56 -0300

    feat: not found on app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './components/view/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
