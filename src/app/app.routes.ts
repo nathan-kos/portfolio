b1527fefe9095f17111212ba18169d121f2ce78e
nathan ks <nathanks0210@gmail.com>
2024-11-06 18:44:09 -0300

    feat: home component routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/view/home/home.component';
 import { NotFoundComponent } from './components/view/not-found/not-found.component';
 
 export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
   {
     path: '**',
     component: NotFoundComponent,
