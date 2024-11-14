6505f956961b82e1fc4a6f2dd17685f49ee6c120
nathan ks <nathanks0210@gmail.com>
2024-11-14 15:10:24 -0300

    feat: about me on home componente

--- src/app/components/view/home/home.component.ts
+++ src/app/components/view/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { AboutMeComponent } from '../../resources/about-me/about-me.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AboutMeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
