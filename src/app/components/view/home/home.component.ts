9a0bed9305e94e6680ead4057bb479875d9cd824
nathan ks <nathanks0210@gmail.com>
2024-11-21 12:42:42 -0300

    feat: objective on home component

--- src/app/components/view/home/home.component.ts
+++ src/app/components/view/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from '../../resources/about-me/about-me.component';
+import { ObjectiveComponent } from '../../resources/objective/objective.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutMeComponent],
+  imports: [AboutMeComponent, ObjectiveComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
