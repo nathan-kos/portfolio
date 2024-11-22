cec2232ffb7783bd89b9fff8385dbd8769aad9e8
nathan ks <nathanks0210@gmail.com>
2024-11-22 13:56:03 -0300

    feat: studies component on home component

--- src/app/components/view/home/home.component.ts
+++ src/app/components/view/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutMeComponent } from '../../resources/about-me/about-me.component';
 import { ObjectiveComponent } from '../../resources/objective/objective.component';
+import { StudiesComponent } from '../../resources/studies/studies.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutMeComponent, ObjectiveComponent],
+  imports: [AboutMeComponent, ObjectiveComponent, StudiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
