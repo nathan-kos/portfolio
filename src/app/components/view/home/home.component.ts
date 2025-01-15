02b1e6d659e7bfddc21c1dd4f4fcc7202115bd59
nathan ks <nathanks0210@gmail.com>
2025-01-15 16:16:31 -0300

    feat: projects components on home component

--- src/app/components/view/home/home.component.ts
+++ src/app/components/view/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AboutMeComponent } from '../../resources/about-me/about-me.component';
 import { ExperienceComponent } from '../../resources/experience/experience.component';
 import { ObjectiveComponent } from '../../resources/objective/objective.component';
+import { ProjectsComponent } from '../../resources/projects/projects.component';
 import { StudiesComponent } from '../../resources/studies/studies.component';
 
 @Component({
@@ -12,6 +13,7 @@ import { StudiesComponent } from '../../resources/studies/studies.component';
     ObjectiveComponent,
     StudiesComponent,
     ExperienceComponent,
+    ProjectsComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
