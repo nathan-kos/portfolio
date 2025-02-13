7880d2d7a83e1ca57d216133d514c8d25f897cfc
nathan ks <nathanks0210@gmail.com>
2025-02-13 13:34:11 -0300

    feat: translate stuudies

--- src/app/components/resources/studies/studies.component.ts
+++ src/app/components/resources/studies/studies.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-studies',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, TranslatePipe],
   templateUrl: './studies.component.html',
   styleUrl: './studies.component.css',
 })
