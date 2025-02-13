96f04e764a462fb33735ca5f6a371895c147bbf8
nathan ks <nathanks0210@gmail.com>
2025-02-13 13:04:51 -0300

    feat: translate about me

--- src/app/components/resources/about-me/about-me.component.ts
+++ src/app/components/resources/about-me/about-me.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css',
 })
 export class AboutMeComponent {
+  constructor(private translate: TranslateService){}
+
   public downObjectives() {
     const objectiveComponent = document.getElementById('objectives-section');
     if (objectiveComponent) {
       objectiveComponent.scrollIntoView({ behavior: 'smooth' });
     }
+
+    this.translate.use("pt")
   }
 }
